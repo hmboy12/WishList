58c9d895a4046ac30a41671aa331c2948012e2b7
hmboy12 <126798296+hmboy12@users.noreply.github.com>
2023-12-07 18:41:54 +0100

    Add files via upload

---
+++ wishList.js
@@ -0,0 +1,35 @@
+function omdirigereTilElgigantenLocation(){
+    window.open('https://www.google.com/maps/place/Elgiganten/@55.4891638,8.3161118,11.75z/data=!4m10!1m2!2m1!1selgiganten+esbjerg!3m6!1s0x464b20ff1b9785d1:0x4c0fd7743490c7b2!8m2!3d55.5081053!4d8.4549499!15sChJlbGdpZ2FudGVuIGVzYmplcmciA4gBAVoUIhJlbGdpZ2FudGVuIGVzYmplcmeSARFlbGVjdHJvbmljc19zdG9yZeABAA!16s%2Fg%2F1tdmt6qk?entry=ttu');
+}
+
+function linkTilMusen(){
+    window.open('https://www.elgiganten.dk/product/computer-kontor/computertilbehor/mus-tastatur/musematte/razer-gigantus-v2-musematte-xxl/284095')
+}
+
+function linkTilHeadsæt(){
+    window.open('https://www.elgiganten.dk/product/gaming/spiltilbehor/gaming-headset/razer-blackshark-v2-x-gaming-headset/218074');
+}
+
+function linkTilBukser(){
+    window.open('https://skagen-clothing.dk/products/denim-jeans-black?variant=47383191355715');
+}
+
+function linkTilZalandoBukser(){
+    window.open('https://www.zalando.dk/next-authentic-stretch-jeans-straight-leg-washed-grey-nx322g0lh-c19.html?size=32x29&allophones=0&wmc=SEM450_NB_GO._7584348957_710292843_36624847665.&opc=2211&mpp=google|v1||pla-360989572517||1005278||g|c||163678139002||pla|NX322G0LH-C19003200S|360989572517|1|&gclsrc=aw.ds&gad_source=1&gclid=CjwKCAiA98WrBhAYEiwA2WvhOuvQejfOaoFbfT3RwvbfYmdjf_j_GAxGndqwl5GukKBnLtCMT0IUkxoC2RMQAvD_BwE');
+}
+
+function omdirigereTilJyskLocation(){
+    window.open('https://www.google.com/maps/place/JYSK+Esbjerg/@55.5022914,8.454646,3789m/data=!3m1!1e3!4m10!1m2!2m1!1sJYSK!3m6!1s0x464b20ff68ae464b:0xe855c9ca6c37cb3a!8m2!3d55.5098995!4d8.4554253!15sCgRKWVNLIgOIAQFaBiIEanlza5IBD2Z1cm5pdHVyZV9zdG9yZeABAA!16s%2Fg%2F1tfcrtxb?entry=ttu');
+}
+
+function linkTilLanet(){
+    window.open('https://jysk.dk/sovevaerelse/lagner/facon-glat/kuvertlagen-elenora-140x200x6-10-hvid?gad_source=1&gclid=CjwKCAiA98WrBhAYEiwA2WvhOuNzC-VGhoz5nCHjvV9xZi6sP6PUqkkF-tj08IxV1jGuTP0nE4JbzBoCJkQQAvD_BwE&gclsrc=aw.ds');
+}
+
+function omdirigeringTilHæveautomat(){
+    window.open('https://www.google.com/maps/place/Sydbank+h%C3%A6veautomat/@55.466212,8.4474111,18z/data=!4m11!1m3!2m2!1sh%C3%A6veautomat!6e2!3m6!1s0x464b26d533808a5f:0xd0c05d750ea28874!8m2!3d55.466212!4d8.449533!15sCgxow6Z2ZWF1dG9tYXSSAQNhdG3gAQA!16s%2Fg%2F11c5blfwbz?entry=ttu');
+}
+
+function linkTilMobilePay(){
+    window.open('https://mobilepay.dk/#');
+}
